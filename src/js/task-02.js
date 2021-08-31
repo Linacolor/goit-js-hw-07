c63021b6b17657aa90f7dcd3cfcccec355229b90
Linacolor <annafragile123@gmail.com>
2021-08-31 17:03:30 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -5,4 +5,14 @@ const ingredients = [
   'Помидоры',
   'Зелень',
   'Приправы',
-];
+]
+const arrayOfLi = []
+for (let ingridient of ingredients) {
+  const newLi = document.createElement('li')
+  newLi.textContent = `${ingridient}`
+  arrayOfLi.push(newLi)
+}
+
+const navEl = document.querySelector('ul')
+navEl.append(...arrayOfLi)
+console.log(navEl)
