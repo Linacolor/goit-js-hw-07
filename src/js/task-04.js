95c5d09cde94c91a3b98c6b94bb09045bbbf82a9
Linacolor <annafragile123@gmail.com>
2021-09-03 21:22:15 +0300

    task-04 переименование кнопок

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,12 +1,12 @@
 let counterValue = 0
 
-const incrementEl = document.querySelector('[data-action="increment"]')
-const decrementEl = document.querySelector('[data-action="decrement"]')
+const incrBtnEl = document.querySelector('[data-action="increment"]')
+const decrBtnEl = document.querySelector('[data-action="decrement"]')
 
 const spanEl = document.querySelector('#value')
 
 const increment = () => (spanEl.textContent = counterValue += 1)
 const decrement = () => (spanEl.textContent = counterValue -= 1)
 
-incrementEl.addEventListener('click', increment)
-decrementEl.addEventListener('click', decrement)
+incrBtnEl.addEventListener('click', increment)
+decrBtnEl.addEventListener('click', decrement)
