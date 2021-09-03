ee9e0010fb83d0c6f3188e685c9d3c3079ab24a9
Linacolor <annafragile123@gmail.com>
2021-09-03 21:37:10 +0300

    удаление папки

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const itemEl = document.querySelectorAll('.item')
-const itemTotalQuantity = itemEl.length
-console.log(`В списке ${itemTotalQuantity} категории.`)
-
-for (const item of itemEl) {
-  const itemText = item.querySelector('h2').textContent
-  const liQuantity = item.querySelectorAll('li').length
-  console.log(itemText, liQuantity)
-}
